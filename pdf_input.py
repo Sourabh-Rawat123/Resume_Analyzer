0db0ea87865c941a58a857e47816af51dbb3c0d4
unknown <sourabhrawat231@gmail.com>
2026-06-12 12:35:06 +0530

    second commit

--- pdf_input.py
+++ pdf_input.py
@@ -4,34 +4,38 @@ from pdf_reader import extract_text
 from clean_txt import clean_resume_text
 from prompt import generate_prompt
 from ai_analysis import analyze_resume
+
+
 def pdf_input():
     st.header("AI Resume Analyzer")
 
-    file = st.file_uploader(
-        "Please upload your PDF resume for analysis",
-        type=["pdf"]
-    )
+    file = st.file_uploader("Please upload your PDF resume for analysis", type=["pdf"])
 
     if file is not None:
         st.success("File uploaded successfully")
-        job_title=st.text_area("Enter the job title you are targeting")
+        job_title = st.text_area("Enter the job title you are targeting")
         if st.button("Analyze File"):
             st.write("Analyzing PDF...")
-            text=extract_text(file)
+            text = extract_text(file)
             if text:
-                cleaned_text=clean_resume_text(text)
+                cleaned_text = clean_resume_text(text)
                 if cleaned_text:
-              
-                  system_prompt, user_prompt=  generate_prompt(cleaned_text,job_title)
-                  if system_prompt and user_prompt:
-                    analysis_results=analyze_resume(system_prompt, user_prompt)
-                    if analysis_results:
-                        st.success("Analysis completed successfully")
-                        text=json.loads(analysis_results.text)
-                        st.subheader("Resume Analysis Results")
-                        st.json(text)
-                    else:
-                        st.error("Failed to analyze the resume.")
+
+                    system_prompt, user_prompt = generate_prompt(
+                        cleaned_text, job_title
+                    )
+                    if system_prompt and user_prompt:
+                        analysis_results = analyze_resume(system_prompt, user_prompt)
+                        if analysis_results:
+                            st.success("Analysis completed successfully")
+                            text = json.loads(analysis_results.text)
+                            st.subheader("Resume Analysis Results")
+                            Json_str = json.dumps(text, indent=4)
+                            st.text_area(
+                                "Resume Analysis Results", value=Json_str, height=600 
+                            )
+                        else:
+                            st.error("Failed to analyze the resume.")
                 else:
                     st.error("Failed to clean the resume text.")
             else:
@@ -41,6 +45,7 @@ def pdf_input():
 
     else:
         st.info("Please upload a PDF file.")
+
 
 if __name__ == "__main__":
     pdf_input()
